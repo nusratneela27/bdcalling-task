172d24155fe3f72ce969c332a5b55588c9d7de7e
neela <nusratneela27@gmail.com>
2024-06-29 22:40:30 +0600

    project setup with routes

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+import Home from "../pages/Home/Home";
+import Courses from "../pages/Courses/Courses";
+import Discover from "../pages/Discover/Discover";
+import Login from "../pages/Login/Login";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/courses",
+        element: <Courses></Courses>,
+      },
+      {
+        path: "/discover",
+        element: <Discover></Discover>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+    ],
+  },
+]);
